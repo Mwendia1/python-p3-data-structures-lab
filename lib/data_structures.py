20d7ebc57f6f6ac1a999f76a08b4b82a63bed128
Mwendia1 <elijah.mwendia@student.moringaschool.com>
2025-11-18 21:35:27 +0300

    adds content to index.html

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,48 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
+    return [food["name"] for food in spicy_foods]
     pass
 
 def get_spiciest_foods(spicy_foods):
+    return [food for food in spicy_foods if food["heat_level"] > 5]
     pass
 
 def print_spicy_foods(spicy_foods):
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat = food["heat_level"]
+        heat_emojis = "🌶" * heat
+        print(f"{name} ({cuisine}) | Heat Level: {heat_emojis}")
+
     pass
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+
     pass
 
 def print_spiciest_foods(spicy_foods):
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            heat_emojis = "🌶" * food["heat_level"]
+            print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_emojis}")
+
     pass
 
 def get_average_heat_level(spicy_foods):
+    total = 0
+    for food in spicy_foods:
+        total += food["heat_level"]
+    return total // len(spicy_foods)
+
     pass
 
 def create_spicy_food(spicy_foods, spicy_food):
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
     pass
